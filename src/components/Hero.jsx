984d2aa2d8e48bcfcc8dcae0eeaff94488bbbbc1
Aromal Baby <aromalbaby365@gmail.com>
2025-08-22 21:34:23 +0200

    initial commit with bunch of installed tech stacks for this project like tailwind vite

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div className="relative h-dvh w-screen overflow-x-hidden">
+            <div id="video_frame" className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
+                <div className="mash-clip-path">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Hero
